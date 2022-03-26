226fac7320f07be06487c4f29ca19ddf41537d85
kckl <85543667+kckl@users.noreply.github.com>
2022-03-26 12:57:13 -0700

    api

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from "react"
+import {useEffect, useState} from 'react';
 import OcrReader from "./components/OcrReader"
 import SpotifySender from "./components/SpotifySender"
+import axios from 'axios';
 
-// Spotify API
-const CLIENT_ID = "55ee11e7110b4bfe8d1466e85f42fb78"
-const REDIRECT_URI = "http://localhost:3000/callback"
-const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
-const RESPONSE_TYPE = "token"
+
 
 function App() {
   const [ocrData, setOcrData] = useState("")
+
+  // Spotify API
+  const CLIENT_ID = "55ee11e7110b4bfe8d1466e85f42fb78"
+  const REDIRECT_URI = "http://localhost:3000/callback"
+  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
+  const RESPONSE_TYPE = "token"
+  const [token, setToken] = useState("")
+  const [searchKey, setSearchKey] = useState("")
+  const [artists, setArtists] = useState([])
 
   // Receive OCR data as a prop from the child component
   const onReadOcrData = (ocrData) => {
@@ -34,9 +40,55 @@ function App() {
     setOcrData("")
   }
 
+  // Spotify 
+  useEffect(() => {
+    const hash = window.location.hash
+    let token = window.localStorage.getItem("token")
+
+    if (!token && hash) {
+        token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
+
+        window.location.hash = ""
+        window.localStorage.setItem("token", token)
+    }
+
+    setToken(token)
+
+}, [])
+
+  // logout button 
+  const logout = () => {
+    setToken("")
+    window.localStorage.removeItem("token")
+  }
+
+  const searchArtists = async (e) => {
+    e.preventDefault()
+    const {data} = await axios.get("https://api.spotify.com/v1/search", {
+        headers: {
+            Authorization: `Bearer ${token}`
+        },
+        params: {
+            q: searchKey,
+            type: "artist"
+        }
+    })
+
+    setArtists(data.artists.items)
+  }
+
+  const renderArtists = () => {
+    return artists.map(artist => (
+      <div key={artist.id}>
+          {artist.images.length ? <img width={"100%"} src={artist.images[0].url} alt=""/> : <div>No Image</div>}
+          {artist.name}
+      </div>
+    ))
+  }
+
   return (
     <div className="App">
-      <header>Welcome to the OCR app!</header>
+      <header>Festival Playlist Generator</header>
       <OcrReader
         onReadOcrData={onReadOcrData}
         onRemoveClicked={onRemoveClicked}
@@ -44,7 +96,20 @@ function App() {
       {ocrData}
 
     {/* Login to Spotify */}
-      <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login to Spotify</a>
+    {!token ?
+          <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login
+          to Spotify</a>
+          : <button onClick={logout}>Logout</button>}
+
+    {token ?
+          <form onSubmit={searchArtists}>
+              <input type="text" onChange={e => setSearchKey(e.target.value)}/>
+              <button type={"submit"}>Search</button>
+          </form>
+          : <h2>Please login</h2>
+        }
+
+      {renderArtists()}
     </div>
   )
 }
