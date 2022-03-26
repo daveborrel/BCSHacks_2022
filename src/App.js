290a2369b684f836bb23d6dae68997c311246376
kckl <85543667+kckl@users.noreply.github.com>
2022-03-26 09:10:45 -0700

    added OcrReader

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import { useState } from "react"
+import OcrReader from "./components/OcrReader"
+import SmsSender from "./components/SpotifySender"
 
 function App() {
+  const [ocrData, setOcrData] = useState("")
+
+  // Receive OCR data as a prop from the child component
+  const onReadOcrData = (ocrData) => {
+    setOcrData(ocrData)
+  }
+
+  // Prop detects that the change image button was clicked
+  const onRemoveClicked = () => {
+    setOcrData("")
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header>Welcome to the OCR app!</header>
+      <OcrReader
+        onReadOcrData={onReadOcrData}
+        onRemoveClicked={onRemoveClicked}
+      />
+      {ocrData}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
